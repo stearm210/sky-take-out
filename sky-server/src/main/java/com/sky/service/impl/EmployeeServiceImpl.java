903e952f2e00b778fea994d0af30860b9e314d78
stearm210 <pyzxwoo@163.com>
2024-08-23 11:23:36 +0800

    苍穹外卖初始代码

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
@@ -14,6 +15,8 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -66,14 +69,27 @@ public class EmployeeServiceImpl implements EmployeeService {
     * */
     @Override
     public void save(EmployeeDTO employeeDTO) {
+        /*
+        * 这里将新增员工的所有信息进行定义，分别进行赋值等操作
+        * */
+
         //由于前端传入的类型与实际后端需要进行操作的类型是不一致的，因此需要进行强制类型转换
         Employee employee=new Employee();
+
         //使用对象的属性拷贝将数据批量的进行赋值
         //这样的前提是属性名是必须一致的前提
         BeanUtils.copyProperties(employeeDTO,employee);
 
         //设置账号的状态，默认是正常的,这里调用了常量类
         employee.setStatus(StatusConstant.ENABLE);
+
+        //设置密码，默认的密码是123456
+        //这个密码要放到数据库中，因此这个密码需要进行加密
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+        //设置当前记录的创建时间和修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
 
     }
 
