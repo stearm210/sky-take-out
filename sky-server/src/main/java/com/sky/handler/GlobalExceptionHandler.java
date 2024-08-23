ac1e7c7972608a1cb154a3e7da9e5edcb1de8559
stearm210 <pyzxwoo@163.com>
2024-08-23 15:51:21 +0800

    苍穹外卖初始代码

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
  */
@@ -24,4 +26,22 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    /*
+    * 主要用于处理sql异常
+    * */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //例子：Duxxxxx enxxx 'zhangsan' for key xxxx
+        //1.先获取异常信息
+        String message = ex.getMessage();
+        //2.判断有没有关键字
+        if (message.contains("Duplicate entry")){
+            //根据空格进行分割
+            String[] split = message.split(" ");
+            //希望通过报错信息获得对应名字
+            String username = split[2];
+            String msg=username+"已存在";
+            return Result.error(msg);
+        }
+    }
 }
