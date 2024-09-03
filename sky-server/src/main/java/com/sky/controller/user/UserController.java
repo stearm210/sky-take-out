10ecfc10f8e834c19e336e5ce6244e268d6819a3
stearm210 <pyzxwoo@163.com>
2024-09-03 19:16:49 +0800

    微信小程序配置信息

--- sky-server/src/main/java/com/sky/controller/user/UserController.java
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -26,6 +26,7 @@ import java.util.Map;
 
 @RestController
 //使用request接口
+//使用request进行请求服务
 @RequestMapping("/user/user")
 @Api(tags = "C端用户常用接口")
 @Slf4j
@@ -42,6 +43,7 @@ public class UserController {
 	* */
 	@PostMapping("/login")
 	@ApiOperation("微信登录操作")
+	//返回一个json文件
 	public Result<UserLoginVO> login(@RequestBody UserLoginDTO userLoginDTO){
 		log.info("微信用户登录:{}",userLoginDTO.getCode());
 
@@ -52,10 +54,12 @@ public class UserController {
 		//为微信用户生成jwt令牌
 		//用户之唯一标识
 		Map<String, Object> claims = new HashMap<>();
+		//使用map集合放入得到的userid
 		claims.put(JwtClaimsConstant.USER_ID, user.getId());
 		//生成对应的令牌,这里将claims进行注入拼接
 		String token = JwtUtil.createJWT(jwtProperties.getUserSecretKey(),jwtProperties.getUserTtl(),claims);
 
+		//创建一个userloginvo已经定义好模版
 		UserLoginVO userLoginVO = UserLoginVO.builder()
 				.id(user.getId())
 				.openid(user.getOpenid())
