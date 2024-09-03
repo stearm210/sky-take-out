67777ca9c5c51577073085611a92c75ee257321d
stearm210 <pyzxwoo@163.com>
2024-09-03 10:56:31 +0800

    微信小程序配置信息

--- sky-common/src/main/java/com/sky/properties/WeChatProperties.java
+++ sky-common/src/main/java/com/sky/properties/WeChatProperties.java
@@ -4,7 +4,9 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
+/*
+* 微信小程序的配置问题
+* */
 @Component
 @ConfigurationProperties(prefix = "sky.wechat")
 @Data
