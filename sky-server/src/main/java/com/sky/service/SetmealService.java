1359edb9894de00d5a8b69702203e18014ca86cb
stearm210 <pyzxwoo@163.com>
2024-09-04 20:35:12 +0800

    套餐操作

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -3,6 +3,9 @@ package com.sky.service;
 import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.result.PageResult;
+import com.sky.vo.SetmealVO;
+
+import java.util.List;
 
 public interface SetmealService {
 
@@ -21,5 +24,24 @@ public interface SetmealService {
 	 */
 	PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 
+	/**
+	 * 批量删除套餐
+	 * @param ids
+	 */
+	void deleteBatch(List<Long> ids);
 
+	/*
+	* 根据id查询套餐和关联的菜品数据
+	* */
+	SetmealVO getByIdWithDish(Long id);
+
+	/*
+	* 修改套餐
+	* */
+	void update(SetmealDTO setmealDTO);
+
+	/*
+	* 套餐的起售停售
+	* */
+	void startOrStop(Integer status, Long id);
 }
